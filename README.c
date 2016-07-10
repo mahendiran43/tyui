cad03631156fa7c7387d4a5364012abbdd72e728
mahendiran43 <kmahee000@gmail.com>
2016-07-10 14:56:20 +0530

    finish

---
+++ README.c
@@ -0,0 +1,25 @@
+# tyui
+include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,x,y,c,d;
+clrscr();
+printf("A robot have 2 type of commands\n");
+scanf("%d",&n);
+for(i=0;i<=x;i++)
+{
+if(x>=0 && x<=10000)
+{
+ printf("command1:move forward%d",x);
+}
+}
+for(i=0;i<=y;i++)
+{
+if(y==-180 && y==180)
+{
+printf("command2:Rotate by X degree:%d",y);
+}
+}
+getch();
+}
